57ba5d9712f4ff05d110a6cd8c5d7fa0c57696e3
Gayathri <gayathri6@gatech.edu>
2012-10-26 05:31:54 -0400

    Create Profile changes

--- ParseStarterProject/gen/com/parse/starter/R.java
+++ ParseStarterProject/gen/com/parse/starter/R.java
@@ -23,17 +23,24 @@ public final class R {
         public static final int sickicon=0x7f020009;
     }
     public static final class id {
+        public static final int ScrollViewProfile=0x7f060005;
+        public static final int allergies=0x7f060006;
         public static final int btnLogin=0x7f060003;
-        public static final int btnRegister=0x7f060008;
+        public static final int btnProfile=0x7f06000b;
+        public static final int btnRegister=0x7f06000f;
+        public static final int dob=0x7f06000a;
         public static final int header=0x7f060000;
-        public static final int link_to_login=0x7f060009;
+        public static final int height=0x7f060008;
+        public static final int link_to_login=0x7f060010;
         public static final int link_to_register=0x7f060004;
         public static final int login_password=0x7f060002;
         public static final int login_username=0x7f060001;
-        public static final int menu_settings=0x7f06000a;
-        public static final int reg_email=0x7f060006;
-        public static final int reg_password=0x7f060007;
-        public static final int reg_username=0x7f060005;
+        public static final int longTermIllness=0x7f060007;
+        public static final int menu_settings=0x7f060011;
+        public static final int reg_email=0x7f06000d;
+        public static final int reg_password=0x7f06000e;
+        public static final int reg_username=0x7f06000c;
+        public static final int weight=0x7f060009;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
@@ -50,10 +57,14 @@ public final class R {
         public static final int activity_register=0x7f050004;
     }
     public static final class string {
+        public static final int allergies=0x7f04000d;
         public static final int app_name=0x7f040001;
+        public static final int dob=0x7f040011;
         public static final int email=0x7f040008;
+        public static final int height=0x7f04000f;
         public static final int hello=0x7f040000;
         public static final int hello_world=0x7f040002;
+        public static final int longTermIllness=0x7f04000e;
         public static final int menu_settings=0x7f040003;
         public static final int password=0x7f040009;
         public static final int title_activity_home=0x7f040006;
@@ -61,7 +72,9 @@ public final class R {
         public static final int title_activity_profile=0x7f04000b;
         public static final int title_activity_record_symptom=0x7f04000c;
         public static final int title_activity_register=0x7f040005;
+        public static final int user=0x7f040012;
         public static final int username=0x7f040007;
+        public static final int weight=0x7f040010;
         public static final int welcome=0x7f04000a;
     }
 }
--- ParseStarterProject/src/com/parse/starter/ProfileActivity.java
+++ ParseStarterProject/src/com/parse/starter/ProfileActivity.java
@@ -1,8 +1,14 @@
 package com.parse.starter;
 
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class ProfileActivity extends Activity {
 
@@ -10,11 +16,41 @@ public class ProfileActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+       
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_profile, menu);
         return true;
+    }
+    
+    public void createProfile(View view){
+    	
+    	EditText editTextAllergies = (EditText) findViewById(R.id.allergies);
+        String allergies = editTextAllergies.getText().toString();
+        
+        EditText editTextLongTermIllness = (EditText) findViewById(R.id.longTermIllness);
+        String longTermIllness = editTextLongTermIllness.getText().toString();
+    	
+        EditText editTextHeight = (EditText) findViewById(R.id.height);
+        String height = editTextHeight.getText().toString();
+    	
+        EditText editTextWeight = (EditText) findViewById(R.id.weight);
+        String weight = editTextWeight.getText().toString();
+    	
+        DatePicker dobWidget = (DatePicker) findViewById(R.id.dob);
+        String dob = dobWidget.toString();
+        
+        ParseObject myProfile = new ParseObject("Profile");
+        myProfile.put(getResources().getString(R.string.allergies), allergies);
+        myProfile.put(getResources().getString(R.string.longTermIllness), longTermIllness);
+        myProfile.put(getResources().getString(R.string.height), height);
+        myProfile.put(getResources().getString(R.string.weight), weight);
+        myProfile.put(getResources().getString(R.string.dob), dob);
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        myProfile.put(getResources().getString(R.string.user), currentUser);
+        myProfile.saveInBackground();
+        
     }
 }
